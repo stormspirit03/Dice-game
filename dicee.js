22d0ab222af0b40f9e8dd9368907dc1502fb2089
Raviraj Gardi <53505985+stormspirit03@users.noreply.github.com>
2021-12-03 15:28:33 +0530

    Add files via upload

---
+++ dicee.js
@@ -0,0 +1,29 @@
+var number=Math.floor(Math.random()*6)+1;  
+
+var diceNumber="images/dice"+number+".png";
+
+var image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", diceNumber);
+
+
+
+number2=Math.floor(Math.random()*6)+1;  
+var diceNumber2="images/dice"+number2+".png";
+
+var image2=document.querySelectorAll('img')[1];
+
+image2.setAttribute("src", diceNumber2);
+
+if (number>number2)
+{
+    document.querySelector("h1").innerHTML="Player1 won";
+}
+
+else if(number<number2)
+{
+    document.querySelector('h1').innerHTML="Player2 won "
+}
+
+else{
+    document.querySelector("h1").innerHTML="Draw"
+}
